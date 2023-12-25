8de5f4dfecf84d4e28e70c31a6676b48db6d0b15
Matteo Depascale <m.depascale@reply.it>
2023-12-25 10:13:20 +0100

    feat: documentation setup

---
+++ packages/functions/src/postman/index.ts
@@ -0,0 +1,12 @@
+import { type Handler, middyfy } from '@core/libs/middyWrapper'
+
+import collection from './NewsletterManager.postman_collection.json'
+
+const main: Handler<void, void, void> = async (event) => {
+  return {
+    statusCode: 200,
+    body: { ...collection }
+  }
+}
+
+export const handler = middyfy(main)
